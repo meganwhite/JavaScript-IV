574bf7e9814c4170d4cd89504a237a5371c1e703
meganwhite <mfw247@gmail.com>
2019-06-13 16:36:27 -0400

    built Person and Instructor classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,13 +2,27 @@
 
 class Person {
     constructor(atts) {
-
+        this.name = atts.name;
+        this.age = atts.age;
+        this.location = atts.location;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
 }
 
 class Instructors extends Person {
     constructor(atts) {
         super(atts);
+        this.speciality = atts.speacialty;
+        this.favLanguage = atts.favLanguage;
+        this.catchPhrase = atts.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student,subject) {
+        return `${student} receives a perfect score on ${subject}`;
     }
 }
 
