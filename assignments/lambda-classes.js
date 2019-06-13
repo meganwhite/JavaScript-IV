9add1649e052cbccce9d39e5672dc4b7b53141b4
meganwhite <mfw247@gmail.com>
2019-06-13 16:31:19 -0400

    built skeleton for lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,25 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(atts) {
+
+    }
+}
+
+class Instructors extends Person {
+    constructor(atts) {
+        super(atts);
+    }
+}
+
+class Students extends Person {
+    constructor(atts) {
+        super(atts);
+    }
+}
+
+class ProjectManagers extends Instructors {
+    constructor(atts){
+        super(atts);
+    }
+}
